5cfc10917ed5d07e527346cb8cc3f6247f0c52e0
Str0ng1ch <artemkuzmi@gmail.com>
2023-10-22 13:28:38 +0300

    Сделал простую регистрацию, login и logout

--- bunker/bunker_game/models.py
+++ bunker/bunker_game/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    nickname = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
+    premium = models.BooleanField()
+
+
+class Card(models.Model):
+    card_type = models.CharField(max_length=15)
+    card_text_ru = models.CharField(max_length=50)
+    card_text_en = models.CharField(max_length=50)
+    profession_ability = models.CharField(max_length=50)
+    card_photo = models.CharField(max_length=50)
+    card_rating = models.SmallIntegerField()
+    premium = models.BooleanField()
+
+
+class Room(models.Model):
+    room_link = models.CharField(max_length=50)
+    premium = models.BooleanField()
+
+
+class Player(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=50)
