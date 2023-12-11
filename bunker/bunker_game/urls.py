4035964234a72e857717a95db70821ef28eb6062
Str0ng1ch <artemkuzmi@gmail.com>
2023-12-11 03:26:35 +0300

    Добавил составление задач и их проверку

--- bunker/bunker_game/urls.py
+++ bunker/bunker_game/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("register/", views.register_page, name="register"),
     path("login/", views.login_page, name="login"),
     path("logout/", views.logout_user, name="logout"),
+    path('created_version/', views.created_version, name='created_version'),
+    path('generate_version/', views.generate_version, name='generate_version'),
+    path('check_results/', views.check_results, name='check_results'),
 ]
--- bunker/bunker_game/views.py
+++ bunker/bunker_game/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
 
 from .forms import CreateUserForm
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def generate_version(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    return render(request, 'generate_version.html')
 
 
 def register_page(request):
@@ -41,5 +48,47 @@ def login_page(request):
 
 
 def logout_user(request):
-    logout(request)
+    if request.user.is_authenticated:
+        logout(request)
     return redirect('login')
+
+
+def create_tasks(needed_tasks):
+    tasks = []
+
+    for key, value in needed_tasks.items():
+        for i in range(int(value)):
+            tasks.append([f"Условие для {key}_{i}", i])
+
+    return tasks
+
+
+def created_version(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    if request.method == 'POST':
+        fields = ['planimetria', 'vectors', 'stereometry']
+        needed_tasks = {field: request.POST.get(field) for field in fields}
+
+        tasks = create_tasks(needed_tasks)
+
+        return render(request, 'created_version.html', {'tasks': tasks})
+    else:
+        return HttpResponse("Invalid request")
+
+
+def check_results(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    if request.method == 'POST':
+        results = []
+        for key, value in request.POST.items():
+            if key.startswith('answer_'):
+                task_number = key.split('_')[-1]
+                user_answer = value
+                correct_answer = request.POST.get('correct_answer_' + task_number)
+                results.append((user_answer, correct_answer, user_answer == correct_answer))
+
+        return render(request, 'results.html', {'results': results})
+    else:
+        return HttpResponse("Invalid request")
